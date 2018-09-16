e918adff195c4389b5a69afb4321cf8b9e611752
Nathan <nathanalam99@gmail.com>
2018-09-15 23:42:45 -0400

    key value pairs

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var request = require("request");
 var bodyParser = require("body-parser");
 var $ = require("jquery");
 var param = require("jquery-param");
+var util = require("util");
 
 // path to directory of the app (wheras dirname goes to index.js)
 var path = __dirname;
@@ -28,43 +29,41 @@ app.post("/processImages", function(req, res) {
   for(var i = 0; i < req.body.length; i++) {
     urls.push(Object.entries(req.body, [0])[i][1].str);
   }
-
-  var returnString = processImages(urls);
-  res.send(returnString);
-});
-
-function processImages(urls) {
+  // urls contains an array of urls
+  // pairs will contain key value pairs of urls and their score
   var pairs = {};
 
   for (var x = 0; x < urls.length; x++) {
-      var score = calculateScore(urls[x]);
-      pairs[urls[x]] = score;
+      // we will make an ajax request for each of the scoredImages
+      var options = { method: 'POST',
+      url: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect',
+      qs:
+       { returnFaceId: 'true',
+         returnFaceLandmarks: 'false',
+         returnFaceAttributes: 'smile,emotion,blur,exposure,noise' },
+      headers:
+       { 'Postman-Token': 'f3220796-ee82-4ae7-bfbb-70745c0cf1f6',
+         'Cache-Control': 'no-cache',
+         'Ocp-Apim-Subscription-Key': 'abe8bd6bf09d46399f42bcb27097d98c',
+         'Content-Type': 'application/json',
+         'GIVEHACK' : "******" + urls[x] + "******"},
+      body: { url: urls[x]},
+      json: true };
+
+      // this will be what we have to wait for each time
+      request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        var identifier = JSON.stringify(util.inspect(response.client._httpMessage.res.req)).split("******")[1];
+        // populate the pairs with the key value pairs
+        pairs[identifier] = getImageScore(body);
+        // check if the pairs is the max length. If so, res.send it
+        if(pairs.length == urls.length) {
+          console.log(pairs);
+          res.send("urls[indexOfMax(sums)]");
+        }
+      });
   }
-
-  console.log(pairs);
-  return "urls[indexOfMax(sums)]";
-};
-
-function calculateScore(imageUrl) {
-    var options = { method: 'POST',
-    url: 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect',
-    qs:
-     { returnFaceId: 'true',
-       returnFaceLandmarks: 'false',
-       returnFaceAttributes: 'smile,emotion,blur,exposure,noise' },
-    headers:
-     { 'Postman-Token': 'f3220796-ee82-4ae7-bfbb-70745c0cf1f6',
-       'Cache-Control': 'no-cache',
-       'Ocp-Apim-Subscription-Key': 'abe8bd6bf09d46399f42bcb27097d98c',
-       'Content-Type': 'application/json' },
-    body: { url: imageUrl},
-    json: true };
-
-  request(options, function (error, response, body) {
-    if (error) throw new Error(error);
-    return getImageScore(body);
-  });
-}
+});
 
 function getImageScore(data) {
     // Show formatted JSON on webpage.
