7c26d15c396b18422419f63e85751e1e21c34797
Nathan <nathanalam99@gmail.com>
2018-09-16 06:17:34 -0400

    added facebook album id support

--- public/javascript/process.js
+++ public/javascript/process.js
@@ -1,5 +1,9 @@
+var fbtoken = "EAAKGuI6tD2gBABnxCSrTsmZAZBvEZAs5LdvFdOQL0acbhBt2jNTW2YNhBd1OaG6njKZACHPCVAbL0QtD4aLpOJbfNXuxrSEPUhZAHYRUAEheBtChomL5JBLZAJrExPrqxjZArkjmbaFfi6PAMlJ1k61SlPCTBJSLeYtK0a9rhXnUq2h92nLrUsXOrahFGv8y1cbKtkI5NNxZBgZDZD";
+var FBMode = true;
+
 var inputCount = 0;
 function addPictureInput() {
+  FBMode = false;
   $("#facebookInput").remove();
   var input = document.createElement("input");
   input.id = "input" + inputCount;
@@ -16,25 +20,81 @@ function addPictureInput() {
 
 function submitPictures() {
   var urls = [];
-  for(var i = 0; i < inputCount; i++) {
-    var str = document.getElementById("input" + i).value;
-    urls.push({str, i});
-  }
-  // urls should have all of the submitted urls
-  $.ajax({
-    url: '/processImages',
-    type: 'POST',
-    contentType: 'application/json',
-    data: JSON.stringify(urls),
-    success: function(data){
-      var list = JSON.parse(data);
-      var urls = Object.keys(list).sort(function(a,b){return list[b]-list[a]})
-      for(var i = 0; i < urls.length; i++) {
-        createCard(urls[i], "Face Score: " + JSON.parse(data)[urls[i]]);
-      }
-
+  if(!FBMode) {
+    for(var i = 0; i < inputCount; i++) {
+      var str = document.getElementById("input" + i).value;
+      urls.push({str, i});
     }
-  });
+    // urls should have all of the submitted urls
+    $.ajax({
+      url: '/processImages',
+      type: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify(urls),
+      success: function(data){
+        var list = JSON.parse(data);
+        var urls = Object.keys(list).sort(function(a,b){return list[b]-list[a]})
+        for(var i = 0; i < urls.length; i++) {
+          createCard(urls[i], "Face Score: " + JSON.parse(data)[urls[i]]);
+        }
+
+      }
+    });
+  } else  {
+    // the FB method - 1095410460622137
+    var album = $("#facebookURL").val();
+    var settings = {
+      "async": true,
+      "crossDomain": true,
+      "url": "https://graph.facebook.com/v3.1/" + album + "/photos?access_token=" + fbtoken,
+      "method": "GET",
+      "headers": {
+        "Cache-Control": "no-cache",
+        "Postman-Token": "b5b9eaf6-c50a-4c7d-a000-f0efdff54653"
+      }
+    }
+
+    $.ajax(settings).done(function (response) {
+      if(response.data) {
+        for(var e = 0; e < response.data.length; e++) {
+          console.log(e);
+          var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://graph.facebook.com/" + response.data[e].id + "?access_token=" + fbtoken + "&fields=images",
+            "method": "GET",
+            "headers": {
+              "Cache-Control": "no-cache",
+              "Postman-Token": "66bbae29-0527-46c2-93b7-24d3f77a102b"
+            }
+          }
+
+          $.ajax(settings).done(function (response) {
+            var str = response.images[0].source;
+            urls.push({str, e});
+          });
+        }
+        console.log(urls);
+        // urls should have all of the submitted urls
+        $.ajax({
+          url: '/processImages',
+          type: 'POST',
+          contentType: 'application/json',
+          data: JSON.stringify(urls),
+          success: function(data){
+            var list = JSON.parse(data);
+            var urls = Object.keys(list).sort(function(a,b){return list[b]-list[a]})
+            for(var i = 0; i < urls.length; i++) {
+              createCard(urls[i], "Face Score: " + JSON.parse(data)[urls[i]]);
+            }
+
+          }
+        });
+      }
+    });
+  }
+
+
 
 }
 
